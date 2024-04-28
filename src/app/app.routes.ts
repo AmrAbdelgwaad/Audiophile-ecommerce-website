4e97dd127beaaab6644936bce95f4d0f1b36017f
AmrAbdelgwaad <115653887+AmrAbdelgwaad@users.noreply.github.com>
2024-04-28 18:15:32 +0300

    Adding routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    title: 'Audiophile | Home',
+    loadComponent: () =>
+      import('./home/home.component').then(
+        (m) => m.HomeComponent,
+      ),
+  },
+  {
+    path: 'headphones',
+    loadComponent: () =>
+      import('./headphones/headphones.component').then(
+        (m) => m.HeadphonesComponent,
+      ),
+    title: 'Audiophile | Headphones',
+  },
+  {
+    path: 'speakers',
+    loadComponent: () =>
+      import('./speakers/speakers.component').then((m) => m.SpeakersComponent),
+    title: 'Audiophile | speakers',
+  },
+  {
+    path: 'earphones',
+    loadComponent: () =>
+      import('./earphones/earphones.component').then(
+        (m) => m.EarphonesComponent,
+      ),
+    title: 'Audiophile | earphones',
+  },
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+];
