2e52436f87bef6ad3d2ac4d61a7d39a28797ac94
AmrAbdelgwaad <115653887+AmrAbdelgwaad@users.noreply.github.com>
2024-05-01 18:28:53 +0300

    Navigation finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgOptimizedImage],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Audiophile-ecommerce-website';
+  menuOpened = false;
+
+  toggleMenu() {
+    this.menuOpened = !this.menuOpened;
+  }
 }
